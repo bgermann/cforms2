5bbe8e105230b9e8e2346dcc627d8ec80da52fb3
bgermann <bgermann@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-23 01:45:52 +0000

    version 14.6.7
    git-svn-id: https://plugins.svn.wordpress.org/cforms2/trunk@1012463 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- cforms.php
+++ cforms.php
@@ -1,36 +1,33 @@
 <?php
 /*
-  Copyright (c) 2006-2012 Oliver Seidel (email : oliver.seidel @ deliciousdays.com)
-  Copyright (c) 2014      Matthew Sigley
-  Copyright (c) 2014      Bastian Germann
-
-  This program is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-
-Plugin Name: cforms2
-Plugin URI: https://wordpress.org/plugins/cforms2/
-Description: cformsII offers unparalleled flexibility in deploying contact forms across your blog. Features include: comprehensive SPAM protection, Ajax support, Backup & Restore, Multi-Recipients, Role Manager support, Database tracking and many more.
-Author: Oliver Seidel, Bastian Germann
-Version: 14.6.6
-Text Domain: cforms
-Domain Path: ____Plugin_Localization
-
-
-*/
+ * Copyright (c) 2006-2012 Oliver Seidel (email : oliver.seidel @ deliciousdays.com)
+ * Copyright (c) 2014      Matthew Sigley
+ * Copyright (c) 2014      Bastian Germann
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Plugin Name: cforms2
+ * Plugin URI: https://wordpress.org/plugins/cforms2/
+ * Description: cformsII offers unparalleled flexibility in deploying contact forms across your blog. Features include: comprehensive SPAM protection, Ajax support, Backup & Restore, Multi-Recipients, Role Manager support, Database tracking and many more.
+ * Author: Oliver Seidel, Bastian Germann
+ * Version: 14.6.7
+ * Text Domain: cforms
+ * Domain Path: ____Plugin_Localization
+ */
 
 global $localversion;
-$localversion = '14.6.6';
+$localversion = '14.6.7';
 
 ### db settings
 global $wpdb, $cformsSettings;
