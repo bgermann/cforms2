6ad59c176341c231850bb3f0e88f1e094a26db96
bgermann <bgermann@users.noreply.github.com>
2016-03-05 13:05:29 +0100

    change PHP 5.3 constant to PHP 5.2 compatible one

--- my-functions-deprecated.php
+++ my-functions-deprecated.php
@@ -26,7 +26,7 @@ class cforms2_dreprecated_api {
 
 	public static function my_cforms_action($cformsdata) {
 		if( function_exists('my_cforms_action') ) {
-			trigger_error('You should switch from using my_cforms_action function to cforms2_after_processing_action WordPress action.', E_USER_DEPRECATED);
+			trigger_error('You should switch from using my_cforms_action function to cforms2_after_processing_action WordPress action.', E_USER_WARNING);
 			my_cforms_action($cformsdata);
 		}
 	}
@@ -40,7 +40,7 @@ if (!function_exists('cf_extra_comment_data')) {
 	 * @deprecated since version 14.11.3
 	 */
 	function cf_extra_comment_data( $id ) {
-		trigger_error('cf_extra_comment_data is useless', E_USER_DEPRECATED);
+		trigger_error('cf_extra_comment_data is useless', E_USER_WARNING);
 	}
 }
 
@@ -49,6 +49,6 @@ if (!function_exists('is_tellafriend')) {
 	 * @deprecated since version 14.12
 	 */
 	function is_tellafriend($pid) {
-		trigger_error('is_tellafriend is useless', E_USER_DEPRECATED);
+		trigger_error('is_tellafriend is useless', E_USER_WARNING);
 	}
 }
